72fcc7df048c3c1d5edd946b81dd5f3d63642f5f
Maia Black <padmaia@me.com>
2015-03-22 12:17:50 -0700

    set up postgres

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.get('/', function (req, res) {
   res.render('index', {title: 'THE DUMP'});
 })
 
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
+
 var server = app.listen(process.env.PORT || 5000, function () {
 
   var host = server.address().address
