01d5a6d6f65e47ecd81599c0f3b1a0db77bd3acb
Maia Black <padmaia@me.com>
2015-04-07 00:27:08 -0700

    use bookshelf to connect to db

--- server.js
+++ server.js
@@ -9,19 +9,27 @@ app.get('/', function (req, res) {
   res.render('index', {title: 'THE DUMP'});
 })
 
-var pg = require('pg');
+var knex = require('knex')({
+	client: 'pg',
+	connection: process.env.DATABASE_URL
+});
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
+var bookshelf = require('bookshelf')(knex);
+app.set('bookshelf', bookshelf);
+
+var User = bookshelf.Model.extend({
+  tableName: 'users'
+});
+
+app.get('/db', function(req, res) {
+  new User().fetchAll()
+    .then(function(users) {
+      res.send(users.toJSON());
+    }).catch(function(error) {
+      console.log(error);
+      res.send('An error occured');
     });
-  });
-})
+});
 
 var server = app.listen(process.env.PORT || 5000, function () {
 
